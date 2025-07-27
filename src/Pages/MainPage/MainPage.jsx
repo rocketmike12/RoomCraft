aade1f9f0fadd92939d36ad1c1dd5ca5f8e78731
Miha77777ua <lv11miha@gmail.com>
2025-07-27 16:53:10 +0300

    test

--- src/Pages/MainPage/MainPage.jsx
+++ src/Pages/MainPage/MainPage.jsx
@@ -1,10 +1,14 @@
 import { Header } from "../../components/Header/Header";
+import { Catalog } from "../../components/Catalog/Catalog";
+import style from "./MainPage.module.scss";
 
 export const MainPage = () => {
   return (
     <>
       <Header />
-      <main></main>
+      <main className={style.main}>
+        <Catalog />
+      </main>
     </>
   );
 };
