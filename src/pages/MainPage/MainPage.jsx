1f4de25a623d33a5d3eaeaa503b7c1483e55c453
Yulia Pedchenko <pedcenkoulia2@gmail.com>
2025-07-29 20:52:04 +0300

    Canvas style

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -3,47 +3,47 @@ import { WelcomeModal } from "../../components/WelcomeModal/WelcomeModal";
 import { Header } from "../../components/Header/Header";
 import { Palette } from "../../components/Palette/Palette";
 import style from "./MainPage.module.scss";
-import { Canvas, addObject, renderCanvas } from "../../components/Canvas/Canvas.jsx";
+import {
+  Canvas,
+  addObject,
+  renderCanvas,
+} from "../../components/Canvas/Canvas.jsx";
 import { Catalog } from "../../components/Catalog/Catalog.jsx";
 import { Question } from "../../components/Question/Question.jsx";
 import { Container } from "../../components/Container/Container.jsx";
 
 export const MainPage = () => {
-	const [showModal, setShowModal] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
-	const canvasRef = useRef();
+  const canvasRef = useRef();
 
-	const handleAdd = (id) => {
-		addObject(id);
+  const handleAdd = (id) => {
+    addObject(id);
 
-		const canvas = canvasRef.current;
-		if (!canvas) return;
+    const canvas = canvasRef.current;
+    if (!canvas) return;
 
-		const ctx = canvas.getContext("2d");
-		if (!ctx) return;
+    const ctx = canvas.getContext("2d");
+    if (!ctx) return;
 
-		renderCanvas(ctx, canvas);
-	};
+    renderCanvas(ctx, canvas);
+  };
 
-	useEffect(() => {
-		if (!localStorage.getItem("visitedRoomCraft")) {
-			setShowModal(true);
-		}
-	}, []);
+  useEffect(() => {
+    if (!localStorage.getItem("visitedRoomCraft")) {
+      setShowModal(true);
+    }
+  }, []);
 
-	const [selectedColor, setSelectedColor] = useState(null);
-	return (
-		<>
-			{showModal && <WelcomeModal onClose={() => setShowModal(false)} />}
-			<Header />
-			<main className={style.main}>
-				<Container>
-					<Catalog onAdd={handleAdd} />
-					<Canvas color={selectedColor} canvasRef={canvasRef} />
-					<Palette selectedColor={selectedColor} setSelectedColor={setSelectedColor} />
-					<Question />
-				</Container>
-			</main>
-		</>
-	);
+  const [selectedColor, setSelectedColor] = useState(null);
+  return (
+    <>
+      {showModal && <WelcomeModal onClose={() => setShowModal(false)} />}
+      <Header />
+      <main className={style.main}>
+        <Catalog onAdd={handleAdd} />
+        <Canvas canvasRef={canvasRef} />
+      </main>
+    </>
+  );
 };
