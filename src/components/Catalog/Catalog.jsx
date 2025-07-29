fdc571bb4b7559e1efea5f21d3049f515de69a2b
rocketmike12 <mg.korop@gmail.com>
2025-07-29 17:07:15 +0300

    integrated canvas & catalog

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -1,60 +1,77 @@
-import { useState } from "react";
-import Search from "../../img/search.svg";
+import { useState, useRef, useEffect } from "react";
+import { addObject } from "../Canvas/Canvas.jsx";
 import style from "./Catalog.module.scss";
 
 import data from "../../data/images.js";
 
-export const Catalog = () => {
-  const [activeCategory, setActiveCategory] = useState("Всі");
+export const Catalog = ({ onAdd }) => {
+	const [activeCategory, setActiveCategory] = useState("Всі");
 
-  return (
-    <div className={style.catalog}>
-      <h2 className={style.catalog__title}>Каталог</h2>
-      <ul className={style.catalog__categories}>
-        <li
-          className={`${style.categories__category}${activeCategory === "Всі" ? ` ${style.active}` : ""}`}
-        >
-          <button className={style.categories__btn} onClick={() => setActiveCategory("Всі")}>Всі</button>
-        </li>
-        <li
-          className={`${style.categories__category}${activeCategory === "Стільці" ? ` ${style.active}` : ""}`}
-        >
-          <button className={style.categories__btn} onClick={() => setActiveCategory("Стільці")}>Стільці</button>
-        </li>
-        <li
-          className={`${style.categories__category}${activeCategory === "Столи" ? ` ${style.active}` : ""}`}
-        >
-          <button className={style.categories__btn} onClick={() => setActiveCategory("Столи")}>Столи</button>
-        </li>
-        <li
-          className={`${style.categories__category}${activeCategory === "Шафи" ? ` ${style.active}` : ""}`}
-        >
-          <button className={style.categories__btn} onClick={() => setActiveCategory("Шафи")}>Шафи</button>
-        </li>
-        <li
-          className={`${style.categories__category}${activeCategory === "Тумбочки" ? ` ${style.active}` : ""}`}
-        >
-          <button className={style.categories__btn} onClick={() => setActiveCategory("Тумбочки")}>Тумбочки</button>
-        </li>
-        <li
-          className={`${style.categories__category}${activeCategory === "Дивани" ? ` ${style.active}` : ""}`}
-        >
-          <button className={style.categories__btn} onClick={() => setActiveCategory("Дивани")}>Дивани</button>
-        </li>
-        <li
-          className={`${style.categories__category}${activeCategory === "Ослінчики" ? ` ${style.active}` : ""}`}
-        >
-          <button className={style.categories__btn} onClick={() => setActiveCategory("Ослінчики")}>Ослінчики</button>
-        </li>
-      </ul>
-      <ul className={style.catalog__furniture}>
-        {data.filter(el => activeCategory === "Всі" ? true : el.category === activeCategory).map((el, id) => (
-          <li className={style.furniture__item} key={id} >
-            <img src={el.scaled} alt={el.name} className={style.furniture__img} />
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
-}
+	const catalogRef = useRef(null);
 
+	const handleClick = function (e) {
+		const id = e.target.parentElement?.dataset?.id;
+		if (id == null) return;
+
+		onAdd(Number(id));
+	};
+
+	useEffect(() => {
+		const catalog = catalogRef.current;
+		if (!catalog) return;
+
+		catalog.addEventListener("click", handleClick);
+	}, []);
+
+	return (
+		<div className={style.catalog} ref={catalogRef}>
+			<h2 className={style.catalog__title}>Каталог</h2>
+			<ul className={style.catalog__categories}>
+				<li className={`${style.categories__category}${activeCategory === "Всі" ? ` ${style.active}` : ""}`}>
+					<button className={style.categories__btn} onClick={() => setActiveCategory("Всі")}>
+						Всі
+					</button>
+				</li>
+				<li className={`${style.categories__category}${activeCategory === "Стільці" ? ` ${style.active}` : ""}`}>
+					<button className={style.categories__btn} onClick={() => setActiveCategory("Стільці")}>
+						Стільці
+					</button>
+				</li>
+				<li className={`${style.categories__category}${activeCategory === "Столи" ? ` ${style.active}` : ""}`}>
+					<button className={style.categories__btn} onClick={() => setActiveCategory("Столи")}>
+						Столи
+					</button>
+				</li>
+				<li className={`${style.categories__category}${activeCategory === "Шафи" ? ` ${style.active}` : ""}`}>
+					<button className={style.categories__btn} onClick={() => setActiveCategory("Шафи")}>
+						Шафи
+					</button>
+				</li>
+				<li className={`${style.categories__category}${activeCategory === "Тумбочки" ? ` ${style.active}` : ""}`}>
+					<button className={style.categories__btn} onClick={() => setActiveCategory("Тумбочки")}>
+						Тумбочки
+					</button>
+				</li>
+				<li className={`${style.categories__category}${activeCategory === "Дивани" ? ` ${style.active}` : ""}`}>
+					<button className={style.categories__btn} onClick={() => setActiveCategory("Дивани")}>
+						Дивани
+					</button>
+				</li>
+				<li className={`${style.categories__category}${activeCategory === "Ослінчики" ? ` ${style.active}` : ""}`}>
+					<button className={style.categories__btn} onClick={() => setActiveCategory("Ослінчики")}>
+						Ослінчики
+					</button>
+				</li>
+			</ul>
+			<ul className={style.catalog__furniture}>
+				{data
+					.filter((el) => (activeCategory === "Всі" ? true : el.category === activeCategory))
+					.map((el, id) => (
+						<li className={style.furniture__item} data-id={id} key={id}>
+							<img src={el.scaled} alt={el.name} className={style.furniture__img} />
+						</li>
+					))}
+			</ul>
+		</div>
+	);
+};
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,13 +1,27 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { WelcomeModal } from "../../components/WelcomeModal/WelcomeModal";
 import { Header } from "../../components/Header/Header";
-import { Catalog } from "../../components/Catalog/Catalog";
-import { Canvas } from "../../components/Canvas/Canvas";
 import { Palette } from "../../components/Palette/Palette";
 import style from "./MainPage.module.scss";
+import { Canvas, addObject, renderCanvas } from "../../components/Canvas/Canvas.jsx";
+import { Catalog } from "../../components/Catalog/Catalog.jsx";
 
 export const MainPage = () => {
 	const [showModal, setShowModal] = useState(false);
+
+	const canvasRef = useRef();
+
+	const handleAdd = (id) => {
+		addObject(id);
+
+		const canvas = canvasRef.current;
+		if (!canvas) return;
+
+		const ctx = canvas.getContext("2d");
+		if (!ctx) return;
+
+		renderCanvas(ctx);
+	};
 
 	useEffect(() => {
 		if (!localStorage.getItem("visitedRoomCraft")) {
@@ -21,8 +35,8 @@ export const MainPage = () => {
 			{showModal && <WelcomeModal onClose={() => setShowModal(false)} />}
 			<Header />
 			<main className={style.main}>
-				<Catalog />
-				<Canvas color={selectedColor}/>
+				<Catalog onAdd={handleAdd} />
+				<Canvas color={selectedColor} canvasRef={canvasRef} />
 				<Palette selectedColor={selectedColor} setSelectedColor={setSelectedColor} />
 			</main>
 		</>
